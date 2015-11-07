35c2f8e9bc1ca458ea49d367cb3ff3ed9299b195
Dee <deepakpishe@outlook.com>
2015-11-07 20:31:41 +0000

    telnetlib script

---
+++ 2/telnetlibtest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import telnetlib
+import time
+
+def main():
+    print 'hello'
+    ip = '50.76.53.27'
+    user= 'pyclass'
+    password='88newclass'
+    tport = 23
+    ttimeout = 6
+    remote_conn = telnetlib.Telnet(ip, tport, ttimeout)
+    op = remote_conn.read_until('username:', ttimeout)
+    remote_conn.write(user+'\n')
+    op = remote_conn.read_until('password:', ttimeout)
+    remote_conn.write(password+'\n' )
+    
+    time.sleep(1)
+    op = remote_conn.read_very_eager()    
+
+    print op
+    remote_conn.close()
+
+
+if __name__ == '__main__':
+    main()
