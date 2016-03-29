5bce4e471d70cb8d27c69a1bbc853c517869161f
Dee <deepakpishe@outlook.com>
2016-03-28 22:16:05 -0700

    added map, filter and reduce

--- 5/map.py
+++ 5/map.py
@@ -6,6 +6,13 @@ def asq(x):
 
 print map(asq,a)
 
+#map syntax returns the logical val for the whole set of list
 print map(lambda x: x**2, a)
 
 
+#filter syntax returns the numerical value
+print filter(lambda x: x%2 ==0, a)
+
+#reduce returns the val of operation 
+print reduce(lambda x,y: x+y,a)
+ 
